11485e2cf7e4aa66670bbe393533a914ae32f086
Black <blackng10.5.1997@gmail.com>
2021-02-21 08:57:30 +0700

    'update

--- server.js
+++ server.js
@@ -2,30 +2,64 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+	userJoin,
+	getCurrentUser,
+	userLeave,
+	getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
+const botname = "admin";
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Run when client connect
 io.on("connect", (socket) => {
-	//Welcome only current user
-	socket.emit("message", "Welcome to chatbox");
+	//Listen event join room
+	socket.on("joinRoom", ({ username, room }) => {
+		const user = userJoin(socket.id, username, room);
 
-	// Broadcast when user connect(expect client)
-	socket.broadcast.emit("message", "A User has joined the chat");
+		socket.join(user.room);
+
+		//Welcome only current user
+		socket.emit("message", formatMessage(botname, "Welcome to chatbox"));
+
+		// Broadcast when user connect(expect client)
+		socket.broadcast
+			.to(user.room)
+			.emit(
+				"message",
+				formatMessage(botname, `${user.username} has joined the chat`)
+			);
+
+		//Send user and room info
+		io.to(user.room).emit("roomUser", {
+			room: user.room,
+			users: getRoomUsers(user.room),
+		});
+	});
 
 	//Run when client disconnect
 	socket.on("disconnect", () => {
-		io.emit("message", "A user has left the chat");
+		const user = userLeave(socket.id);
+
+		if (user) {
+			io.to(user.room).emit(
+				"message",
+				formatMessage(botname, `${user.username} has left the chat`)
+			);
+		}
 	});
 
 	//Listen for chatMessage
 	socket.on("chatMessage", (msg) => {
-		console.log(msg);
+		const user = getCurrentUser(socket.id);
+
+		io.to(user.room).emit("message", formatMessage(user.username, msg));
 	});
 });
 
